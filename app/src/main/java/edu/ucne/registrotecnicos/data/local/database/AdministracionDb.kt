8c98fec724f0cb460e0d29d12a5d8385694d01a3
DjMarte <cristopher_marte@ucne.edu.do>
2025-01-18 17:36:24 -0400

    Se cambió el nombre de la base de datos TicketDb a AdministracionDb.

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/AdministracionDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/AdministracionDb.kt
@@ -2,17 +2,23 @@ package edu.ucne.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnicos.data.local.dao.TicketDao
+import edu.ucne.registrotecnicos.data.local.entity.TecnicoEntity
 import edu.ucne.registrotecnicos.data.local.entity.TicketEntity
 
 
 @Database(
     entities = [
+        TecnicoEntity::class,
         TicketEntity::class
     ],
-    version = 1,
+    version = 7,
     exportSchema = false
 )
-abstract class TicketDb:RoomDatabase(){
+@TypeConverters(Converters::class)
+abstract class AdministracionDb : RoomDatabase() {
+    abstract fun tecnicoDao(): TecnicoDao
     abstract fun ticketDao(): TicketDao
 }
