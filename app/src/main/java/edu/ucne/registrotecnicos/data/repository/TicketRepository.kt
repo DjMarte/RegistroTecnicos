60e3749ad3b2351dc5f616fddacb13512ea0270d
DjMarte <cristopher_marte@ucne.edu.do>
2025-01-25 05:11:09 -0400

    Se inyectó el TicketDao en TicketRepository.

--- app/src/main/java/edu/ucne/registrotecnicos/data/repository/TicketRepository.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/repository/TicketRepository.kt
@@ -1,23 +1,20 @@
 package edu.ucne.registrotecnicos.data.repository
 
+import edu.ucne.registrotecnicos.data.local.dao.TicketDao
 import edu.ucne.registrotecnicos.data.local.database.AdministracionDb
 import edu.ucne.registrotecnicos.data.local.entity.TicketEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class TicketRepository @Inject constructor(
-    private val ticketDb: AdministracionDb
+    private val ticketDb: TicketDao
 ) {
-    suspend fun save(ticket: TicketEntity){
-        ticketDb.ticketDao().save(ticket)
-    }
-    suspend fun find(id: Int): TicketEntity?{
-        return ticketDb.ticketDao().find(id)
-    }
-    suspend fun delete(ticket: TicketEntity){
-        return ticketDb.ticketDao().delete(ticket)
-    }
-    fun getAll(): Flow<List<TicketEntity>>{
-        return ticketDb.ticketDao().getAll()
-    }
+    suspend fun save(ticket: TicketEntity) = ticketDb.save(ticket)
+
+    suspend fun find(id: Int): TicketEntity? = ticketDb.find(id)
+
+    suspend fun delete(ticket: TicketEntity) =  ticketDb.delete(ticket)
+
+    fun getAll(): Flow<List<TicketEntity>> = ticketDb.getAll()
+
 }
