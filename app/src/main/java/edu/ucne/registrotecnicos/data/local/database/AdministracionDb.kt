9b7fde73a4af520e5c0a7692ed8d3a9eea2328d6
DjMarte <cristopher_marte@ucne.edu.do>
2025-01-25 22:08:18 -0400

    Cambiando verion en el database.

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/AdministracionDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/AdministracionDb.kt
@@ -3,8 +3,10 @@ package edu.ucne.registrotecnicos.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import edu.ucne.registrotecnicos.data.local.dao.MensajeDao
 import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnicos.data.local.dao.TicketDao
+import edu.ucne.registrotecnicos.data.local.entity.MensajeEntity
 import edu.ucne.registrotecnicos.data.local.entity.TecnicoEntity
 import edu.ucne.registrotecnicos.data.local.entity.TicketEntity
 
@@ -12,13 +14,15 @@ import edu.ucne.registrotecnicos.data.local.entity.TicketEntity
 @Database(
     entities = [
         TecnicoEntity::class,
-        TicketEntity::class
+        TicketEntity::class,
+        MensajeEntity::class
     ],
-    version = 7,
+    version = 1,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class AdministracionDb : RoomDatabase() {
     abstract fun tecnicoDao(): TecnicoDao
     abstract fun ticketDao(): TicketDao
+    abstract fun mensajeDao(): MensajeDao
 }
