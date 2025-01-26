0a7c8c8bbfc72a23e34601043ff396f63d948637
DjMarte <cristopher_marte@ucne.edu.do>
2025-01-25 21:38:17 -0400

    Se agregó la navegación hacia MessageScreen desde TicketScreen.
    Se agregó la ruta para MessageScreen en AdministracionNavHost.

--- app/src/main/java/edu/ucne/registrotecnicos/navigation/AdministracionNavHost.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/navigation/AdministracionNavHost.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import edu.ucne.registrotecnicos.presentation.HomeScreen
+import edu.ucne.registrotecnicos.presentation.messages.MessageScreen
 import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoListScreen
 import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoScreen
 import edu.ucne.registrotecnicos.presentation.tickets.TicketListScreen
@@ -46,13 +47,21 @@ fun AdministracionNavHost(
             val ticketId = it.toRoute<Screen.Ticket>().ticketId
             TicketScreen(
                 ticketId = ticketId,
-                goBackToListScreen = { navHostController.navigateUp() }
+                goBackToListScreen = { navHostController.navigateUp() },
+                goToMessageScreen = { navHostController.navigate(Screen.TicketMessage(ticketId)) }
             )
         }
         composable<Screen.HomeScreen> {
             HomeScreen(
                 goToTecnicoList = { navHostController.navigate(Screen.TecnicoList) },
                 goToTicketList = { navHostController.navigate(Screen.TicketList) }
+            )
+        }
+        composable<Screen.TicketMessage> {
+            val ticketId = it.toRoute<Screen.TicketMessage>().ticketId
+            MessageScreen(
+                ticketId = ticketId,
+                goBackToTicketScreen = { navHostController.navigateUp() }
             )
         }
     }
