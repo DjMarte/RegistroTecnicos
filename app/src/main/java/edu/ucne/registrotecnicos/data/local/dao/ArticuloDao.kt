ed1fd53de9c5dba95604bb935e69a3817ae920d9
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-23 12:11:48 -0400

    Se agregó el Dao con su interface ArticuloDao.

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/dao/ArticuloDao.kt
@@ -0,0 +1,29 @@
+package edu.ucne.registrotecnicos.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.registrotecnicos.data.local.entity.ArticuloEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ArticuloDao {
+    @Upsert
+    suspend fun save(listaArticulo: List<ArticuloEntity>)
+
+    @Query(
+        """
+            Select * From Articulos 
+            Where articuloId = :articuloId
+            Limit 1
+        """
+    )
+    suspend fun find(articuloId: Int): ArticuloEntity?
+
+    @Delete
+    suspend fun delete(articulo: ArticuloEntity)
+
+    @Query("Select * From Articulos")
+    fun getAll(): Flow<List<ArticuloEntity>>
+}
